e8714e4b2744f736e2d1dc19760cfe52e10a2a1e
PhilRandWu <3378814052@qq.com>
2021-10-05 10:55:28 +0800

    链表的递归调用

---
+++ 数据结构之链表/链表的递归遍历.js
@@ -0,0 +1,29 @@
+function Node(value) {
+    this.value = value;
+}
+
+
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+const node6 = new Node(6);
+
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node6;
+
+
+function forEachNode(root) {
+    if(root == null) {
+        return;
+    }
+    console.log(root.value);
+    forEachNode(root.next);
+}
+
+forEachNode(node1);
