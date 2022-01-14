38673614b974f6b15485fdad3c20097aaf552fa1
PhilRandWu <3378814052@qq.com>
2022-01-14 12:03:05 +0800

    二叉树的遍历

---
+++ 算法代码/二叉树之中序遍历.js
@@ -0,0 +1,31 @@
+function Node(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+const A = new Node('A');
+const B = new Node('B');
+const C = new Node('C');
+const D = new Node('D');
+const E = new Node('E');
+const F = new Node('F');
+const G = new Node('G');
+
+A.left = B;
+A.right = C;
+B.left = D;
+B.right = E;
+C.left = F;
+C.right = G;
+
+function forEachNode(root) {
+    if(root === null) {
+        return;
+    }
+    forEachNode(root.left);
+    console.log(root.value);
+    forEachNode(root.right);
+}
+
+forEachNode(A);
---
+++ 算法代码/二叉树的前序遍历.js
@@ -0,0 +1,31 @@
+function Node(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+const A = new Node('a');
+const B = new Node('b');
+const C = new Node('c');
+const D = new Node('d');
+const E = new Node('e');
+const F = new Node('f');
+const G = new Node('g');
+
+A.left = B;
+A.right = C;
+B.left = D;
+B.right = E;
+C.left = F;
+C.right = G;
+
+function forEachNode(root) {
+    if(root === null) {
+        return;
+    }
+    console.log(root.value);
+    forEachNode(root.left);
+    forEachNode(root.right);
+}
+
+forEachNode(A);
